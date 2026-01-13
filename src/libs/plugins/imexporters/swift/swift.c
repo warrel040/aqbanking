cc9a1a8a8c7443a89ffd80eae93ce6c78a5d75f6
Thomas Kriegel <thomasMapper@bin-wieder-da.de>
2026-01-13 14:49:58 +0100

    Additional & corrected translation in de,po
    -  In some .dlg I changed <dialog type="dialog"
    name="ebc_setup_newkeyfile" i18n="aqebics" to [...] i18n="aqbanking"
    -  In libs/plugins/backends/aqgivve/provider_request.c I added I18N and
    its Include to make the translation work
    -  Typo corrected and I18N() added

--- src/libs/plugins/imexporters/swift/swift.c
+++ src/libs/plugins/imexporters/swift/swift.c
@@ -124,7 +124,7 @@ int AH_ImExporterSWIFT_Import(AB_IMEXPORTER *ie,
 
   /* transform DB to transactions */
   GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Debug,
-                       "Data imported, transforming to transactions");
+                       I18N("Data imported, transforming to transactions"));
   rv=AH_ImExporterSWIFT__ImportFromGroup(ctx, dbData, params);
   if (rv) {
     GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error, "Error importing data");
@@ -337,7 +337,7 @@ int AH_ImExporterSWIFT__ImportFromGroup(AB_IMEXPORTER_CONTEXT *ctx,
     if (GWEN_Gui_ProgressAdvance(progressId, GWEN_GUI_PROGRESS_ONE)==
         GWEN_ERROR_USER_ABORTED) {
       GWEN_Gui_ProgressLog(0, GWEN_LoggerLevel_Error,
-                           I18N("Aborted by user"));
+                           I18N("Aborted by user."));
       GWEN_Gui_ProgressEnd(progressId);
       return GWEN_ERROR_USER_ABORTED;
     }
